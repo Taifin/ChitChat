99dbfa7a1c2cc5a7c5685266c76f45a99c4e0043
Taifin <tralllgm@gmail.com>
2022-03-11 22:52:21 +0300

    Docs: commands formats and outputs

--- server/server.h
+++ server/server.h
@@ -11,6 +11,16 @@
 #include "socket.h"
 #include "user.h"
 
+/// +----------+----------------------------+------------------------------------------------------------------------------------------+
+/// | command  |           format           |                                          return                                          |
+/// +----------+----------------------------+------------------------------------------------------------------------------------------+
+/// | login    | login,username,password    | ok: allowed,username; bad: denied,username; no_user_found: none,username; error: dberror |
+/// | register | register,username,password | ok: created,username; duplicate: exists,username                                         |
+/// | connect  | connect,username,password  | ok: connected,username; duplicate: exists,username                                       |
+/// | greet    | hello,username             | Hello, username, I'm Server God!                                                         |
+/// +----------+----------------------------+------------------------------------------------------------------------------------------+
+
+
 namespace sv {
 class controller : network::udp_socket {
     Q_OBJECT
@@ -32,15 +42,22 @@ public:
     void process() override;
 
     void authorize_user(std::vector<std::string> &data,
-                        const network::client &to);
+                        const network::client &to) noexcept;
+    /// Calls to database's authorize(), exceptions are handled.
+    /// Use it to authorize user on login screen.
 
     void register_user(std::vector<std::string> &data,
                        const network::client &to);
+    /// Calls to database's create_user().
+    /// Use it to make new user in database.
 
     void connect_user(std::vector<std::string> &data,
                       const network::client &to);
+    /// Calls to model's connect_user().
+    /// Use it to connect user to the room (in future).
 
     void greet(std::vector<std::string> &data, const network::client &to);
+    /// Debugging: sends message in return.
 
     virtual ~controller(){};
 };
