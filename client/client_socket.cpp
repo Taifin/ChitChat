9cbba5c866841087a0a35ec5aa51072b0139e889
Lily <lilyreber@mail.ru>
2022-03-12 15:46:28 +0300

    created client socket

---
+++ client/client_socket.cpp
@@ -0,0 +1,43 @@
+#include "client_socket.h"
+#include "login.h"
+
+client_socket::client_socket(const QHostAddress &host,
+                             quint16 port,
+                             QObject *parent = nullptr) : udp_socket(host, port, parent) {
+}
+
+void client_socket::process(){
+    std::string status = queries.front().first;
+    network::client current_client = queries.front().second;
+    qDebug() << status.c_str();
+    //case for login
+    if (status == "alowed"){
+        emit run_successful_login();
+    }
+    if (status == "denied"){
+        emit run_wrong_password();
+    }
+    if (status == "none"){
+        emit run_no_user();
+    }
+    if (status == "dberror"){
+        emit run_error();
+    }
+    //case for registration
+    if (status == "alowed"){
+        emit run_successful_login();
+    }
+    if (status == "denied"){
+        emit run_wrong_password();
+    }
+    if (status == "none"){
+        emit run_no_user();
+    }
+    if (status == "dberror"){
+        emit run_error();
+    }
+
+
+}
+
+
---
+++ client/client_socket.h
@@ -0,0 +1,39 @@
+#ifndef CLIENT_SOCKET_H
+#define CLIENT_SOCKET_H
+
+#include <QString>
+#include <QUdpSocket>
+#include <QObject>
+#include "socket.h"
+
+class client_socket : public network::udp_socket
+{
+    Q_OBJECT
+public:
+    client_socket(const QHostAddress &host,
+                  quint16 port,
+                  QObject *parent);
+
+    void process();
+
+signals:
+    void run_successful_login();
+
+    void run_wrong_password();
+
+    void run_no_user();
+
+    void run_error();
+
+    void run_successful_registration();
+
+    void run_duplicate();
+
+    //void run_error();
+
+private:
+    QUdpSocket socket;
+};
+
+#endif // CLIENT_SOCKET_H
+
