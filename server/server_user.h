3ea6455718462249d71f3f0ad9c91a0e5556da3d
Taifin <tralllgm@gmail.com>
2022-03-18 11:06:46 +0300

    server's variant of user

---
+++ server/server_user.h
@@ -0,0 +1,39 @@
+#ifndef CHITCHAT_SERVER_USER_H
+#define CHITCHAT_SERVER_USER_H
+
+#include <utility>
+#include "user.h"
+#include "socket.h"
+
+class server_user : public user {
+public:
+    const network::client client;
+
+    explicit server_user(std::string uname,
+                         std::string upwd,
+                         const network::client& cli,
+                         int x = 0,
+                         int y = 0)
+        : user(std::move(uname), std::move(upwd), x, y), client(cli) {
+    }
+
+    explicit server_user(const ChitChat::user &other,
+                         const network::client& cli) :
+          user(other), client(cli) {
+    }
+
+    explicit server_user(ChitChat::user &&other,
+                         const network::client& cli) :
+          user(other), client(cli) {
+    }
+
+    bool operator==(const server_user &other) {
+        return this->name() == other.name();
+    }
+
+    bool operator==(server_user &&other) {
+        return this->name() == other.name();
+    }
+};
+
+#endif  // CHITCHAT_SERVER_USER_H
