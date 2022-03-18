4ca2ef68ee064ef1b5e22e96ac4afb828a72b9f1
Taifin <tralllgm@gmail.com>
2022-03-18 11:41:34 +0300

    database fixed

--- server_testing.cpp
+++ server_testing.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
-    db::chitchat_database::local_connection();
+    model::database::local_connection();
     sv::controller aboba(QHostAddress::LocalHost, 1234);
 
     return a.exec();
