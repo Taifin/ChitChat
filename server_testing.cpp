7f7cab893e0e2526d237d1d6ab35e83a257ce482
Taifin <tralllgm@gmail.com>
2022-04-10 12:26:13 +0300

    localhost + explanation

--- server_testing.cpp
+++ server_testing.cpp
@@ -3,11 +3,12 @@
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+    // server's running on localhost, sending to port 50000, listening on port 60000
 
     model::database::local_connection();
     network::queries_keeper* keeper = new network::queries_keeper;
-    sv::server_socket receiver(QHostAddress::Any, 60000, keeper);
-    sv::server_socket sender(QHostAddress::Any, 0, keeper);
+    sv::server_socket receiver(QHostAddress::LocalHost, 60000, keeper);
+    sv::server_socket sender(QHostAddress::LocalHost, 0, keeper);
     sv::server_processor processor(keeper, sender);
     std::thread t([&processor](){
        while (true) {
