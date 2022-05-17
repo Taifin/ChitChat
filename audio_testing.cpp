aabc8e6ad4b0df8371e32ddf7aa933379ffb16a0
Taifin <tralllgm@gmail.com>
2022-05-17 17:08:58 +0300

    Cli voice

--- audio_testing.cpp
+++ audio_testing.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     auto *keeper = new network::queries_keeper();
     auto *socket = new QTcpSocket();
-    socket->connectToHost(QHostAddress::LocalHost, 1236);
-    client_socket m(QHostAddress::LocalHost, 1236, socket, keeper, nullptr);
+    socket->connectToHost(QHostAddress("194.169.163.120"), 1236);
+    client_socket m(QHostAddress("194.169.163.120"), 1236, socket, keeper, nullptr);
 
     client::processor processor(keeper, m, socket);
     std::thread t([&processor]() {
