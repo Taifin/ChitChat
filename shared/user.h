a4bb513b09e4556a455ad1590f2cb4e7a0929333
Taifin <tralllgm@gmail.com>
2022-05-28 12:37:33 +0300

    first tries with serialization

--- shared/user.h
+++ shared/user.h
@@ -2,9 +2,10 @@
 #define CHITCHAT_USER_H
 
 #include <utility>
-#include "QHostAddress"
+#include "QObject"
+#include "QDebug"
 #include "string"
-#include "vector"
+#include "message.pb.h"
 
 struct point {
     int x;
@@ -16,11 +17,10 @@ class user : public QObject {
 
     std::string username;
     std::string password;
+    std::string skin = "miku";
     point coords;
 
 public:
-    std::string skin = "miku";
-
     explicit user(std::string uname,
                   std::string upwd,
                   std::string skin_name,
@@ -28,7 +28,7 @@ public:
                   int y = 0)
         : username(std::move(uname)),
           password(std::move(upwd)),
-          skin(skin_name),
+          skin(std::move(skin_name)),
           coords({x, y}){};
 
     user(const user &other)
@@ -64,33 +64,45 @@ public:
         coords = {x, y};
     }
 
-    user &operator=(const user &other) {
-        username = other.username;
-        password = other.password;
-        coords = other.coords;
-        return *this;
-    }
+//    user &operator=(const user &other) {
+//        username = other.username;
+//        password = other.password;
+//        coords = other.coords;
+//        return *this;
+//    }
 
-    std::string get_name() {
+    [[nodiscard]] const std::string& get_name() const {
         return username;
-    }
-
-    void set_name(const std::string &name) {
-        username = name;
     }
 
     [[nodiscard]] std::string get_skin() const {
         return skin;
     }
 
-    void set_skin(const std::string &skin_type) {
-        skin = skin_type;
+    [[nodiscard]] ChitChatMessage::Query serialize(ChitChatMessage::Query::RequestType type) const {
+        ChitChatMessage::Query query;
+        ChitChatMessage::Query_User user;
+        user.set_name(username);
+        user.set_password(password);
+        user.set_skin(skin);
+        user.set_x_coord(coords.x);
+        user.set_y_coord(coords.y);
+        query.set_allocated_user(&user);
+        query.set_rtype(type);
+        return query;
+    }
+
+    void parse(const ChitChatMessage::Query& query) {
+        username = query.user().name();
+        password = query.user().password();
+        skin = query.user().skin();
+        coords = {query.user().x_coord(), query.user().y_coord()};
     }
 
 private slots:
     void initialize(const std::string &name, const std::string &skin_type) {
-        set_skin(skin_type);
-        set_name(name);
+        skin = skin_type;
+        username = name;
         qDebug() << get_skin().c_str() << get_name().c_str();
     }
 };
